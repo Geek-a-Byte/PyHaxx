19a041b622e1826bed1b98559c5fd4dca324e66b
Nazia <ncoder76@gmail.com>
2022-01-15 14:27:51 +0600

    Create lambda.py

---
+++ lambda.py
@@ -0,0 +1,12 @@
+# Program to show the use of lambda functions
+double = lambda x: x * 2
+
+print(double(5))
+
+
+# In the above program, lambda x: x * 2 is the lambda function. Here x is the argument and x * 2 is the expression that gets evaluated and returned.
+
+# This function has no name. It returns a function object which is assigned to the identifier double. We can now call it as a normal function.
+# similar code:
+# def double(x):
+#   return x * 2
